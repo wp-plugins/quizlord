66054cfd93a2d964fc821235f3205e1a9398352a
nahapet_ <nahapet_@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-10-30 07:29:29 +0000

    changed version
    git-svn-id: https://plugins.svn.wordpress.org/quizlord/tags/1.0.1@1016477 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/ql-ajax.js
@@ -0,0 +1,45 @@
+jQuery(document).ready(function($){
+	
+	$(document).on('click', '#start, #next, #finish, #back, #resume, #check', function(){
+		//$(this).action = 'ql_custom_shortcode';
+		var ql_skip = $(this).parent().find("#skip").val();
+		if(typeof $(".qlo:checked").val() != 'undefined' || $(this).attr('name') == 'start' || $(this).attr('name') == 'resume' || $(this).attr('name') == 'back' || ql_skip == 1){
+			$.ajax({
+				type: "POST",
+				url: MyAjax.ajaxurl,
+				data: $(this).parent().serialize() + '&send=' + $(this).attr("value") + '&singul=' + MyAjax.singul,
+				success: function(msg){
+					$(".ql-question").replaceWith(msg.slice(0, -1));
+					if($(this).attr("value") == 'Check'){
+						$("#check").replaceWith('<input type="submit" name="next" value="Next" id="next">');
+					}
+				},
+			});
+		}
+		else{
+			alert("Please check correct answer!");
+		}
+		return false;
+	});
+	
+	var count = $("#ql-time").html();
+
+	var counter = setInterval(timer, 1000);
+
+	function timer(){
+	//console.log(typeof $("#start").val() != 'undefined');
+	  if(typeof $("#start").val() != 'undefined'){
+	  	count = $("div#ql-time").html();
+	  }
+	  else{
+	  	count = count - 1;
+	  }
+	  if (count <= 0){
+	     clearInterval(counter);
+		 $(".ql-question").replaceWith("<h2 class='time-up'>Time is up!</h2>");
+		 $(".ql-time").remove();
+	     return;
+	  }
+	  $("p#ql-time").html(count);
+	}
+});
